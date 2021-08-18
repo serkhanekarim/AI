a7e3b651b79e9b31239361a3d4d0164298befd46
serkhanekarim <karimserkhane@outlook.fr>
2021-08-18 02:10:01 +0200

    add deep learning with speech recognition

---
+++ model_speech_recognition.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import argparse
+import os
+import pathlib
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import tensorflow as tf
+
+from tensorflow.keras.layers.experimental import preprocessing
+from tensorflow.keras import layers
+from tensorflow.keras import models
+from IPython import display
+
+from modules.modeling.speech_recognition import basic_CNN
+
+
+# Set seed for experiment reproducibility
+seed = 42
+tf.random.set_seed(seed)
+np.random.seed(seed)
+
+
+# import argparse
+# import os.path
+
+# from math import *
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# from sklearn.cluster import KMeans
+# from sklearn.model_selection import train_test_split
+
+# from modules.preprocessing.date import DatePreprocessor
+# from modules.preprocessing.feature_encoder import DataEncoder
+# from modules.scraping.scraper import DataScraper
+# from modules.reader.reader import DataReader
+# from modules.visualization.visualization import DataVisualizator
+# from modules.modeling.machine_learning import Modeler
+# from modules.preprocessing.missing_value import DataImputation
+# from modules.preprocessing.feature_scaling import DataScaler
+# from modules.preprocessing.feature_selection import DataSelector
+# from modules.preprocessing.feature_generator import DataGenerator
+# from modules.Global.method import DataMethod
+
+    
+def main(args):
+    '''
+    Ce didacticiel vous montrera comment créer un réseau de reconnaissance vocale de base qui 
+    reconnaît dix mots différents. Il est important de savoir que les vrais systèmes de 
+    reconnaissance vocale et audio sont beaucoup plus complexes, mais comme le MNIST pour les 
+    images, il devrait vous donner une compréhension de base des techniques impliquées. Une fois que 
+    vous aurez terminé ce tutoriel, vous aurez un modèle qui essaie de classer un clip audio d'une 
+    seconde comme "bas", "aller", "gauche", "non", "droit", "stop", "haut " et oui".
+    '''
+    
+    data_directory = args.data_directory
+    size_data_directory = os.path.getsize(data_directory)
+    if size_data_directory <= 4096:
+        tf.keras.utils.get_file(
+            fname=os.path.join(data_directory,'mini_speech_commands.zip'),
+            origin="http://storage.googleapis.com/download.tensorflow.org/data/mini_speech_commands.zip",
+            extract=True,
+            cache_subdir='.',
+            cache_dir=data_directory)
+    
+    commands_directory = os.path.join(data_directory,'mini_speech_commands')
+    
+    commands = np.array(tf.io.gfile.listdir(str(commands_directory)))
+    commands = commands[commands != 'README.md']
+    print('Commands:', commands)
+    
+    filenames = tf.io.gfile.glob(str(commands_directory) + '/*/*')
+    filenames = tf.random.shuffle(filenames)
+    num_samples = len(filenames)
+    print('Number of total examples:', num_samples)
+    [print(path,len(os.listdir(os.path.join(commands_directory,path)))) for path in os.listdir(commands_directory) if path != 'README.md']
+    print('Example file tensor:', filenames[0])
+
+
+
+    # #data_directory = "/home/serkhane/repo/test-quantmetry/data/"
+    # directory_of_script = os.path.dirname(os.path.realpath(__file__))
+    # data_directory = args.data_directory
+    # path_model = args.path_model
+    # mode = args.mode
+    # NaN_imputation_feature_scaling_PCA_usage = args.NaN_imputation_feature_scaling_PCA_usage
+    # max_depth = args.max_depth
+    # eta = args.eta
+    # num_round = args.num_round   
+    
+    # FILENAME_TRAIN = "data_v1.0 (3) (1) (1) (1) (2).csv"    
+    # FEATURES_TO_NLP = ['Instrument(s) joué(s)','Niveau dans ces instruments','Styles musicaux','Villes','Vos disponibilités:','Groupe']
+    # LABEL = "embauche"
+    
+    # path_train = os.path.join(data_directory,FILENAME_TRAIN)
+    
+
+    # # Read data from the file
+    # data = DataReader(path_train).read_data_file()
+
+    # # Print information on variables
+    # DataVisualizator(data).data_inforamtion()    
+    
+    # # Remove two first column which reffers to index, non useful to create model
+    # data.drop(columns=['Unnamed: 0', 'index'], inplace=True)
+    
+    
+    # '''
+    # Feature Engineering/Extraction/Vectorization
+    # '''
+    # # Add external data (gold financial data)
+    # data = DataScraper(data).add_scraped_data("date")
+    # # Feature Engineering on date
+    # data = DatePreprocessor(data).extract_date_information(columns_name="date")
+    # # Convert categorical variable into dummy/indicator variables 
+    # data = DataEncoder(data).dummy_variable()
+    
+    # # Print missing value
+    # DataVisualizator(data).missing_value_plotting()
+    
+    # #Split data into train and test set to evaluate the model
+    # df_train, df_test, df_train_label, df_test_label = train_test_split(data.drop(LABEL,axis=1), data[LABEL], test_size=0.2)
+    
+    # # Missing data imputation using K-NN
+    # imputer = DataImputation(df_train).imputer()
+    # df_train = imputer[0]
+    # # Missing data imputation on test data using training data imputer model
+    # df_test = imputer[1].transform(df_test)
+    # df_test = pd.DataFrame(df_test, columns = df_train.columns)
+    
+    # # Feature Selection before applying scaling data
+    # DataVisualizator(df_train).correlation_matrix() # Correlation Matrix
+    # selector = DataSelector(df_train, df_train_label).features_selection() # Trees
+    # DataVisualizator(df_train).features_importance(estimator=selector[0],
+    #                                                estimator_type='tree',
+    #                                                max_num_features=20)
+    
+    
+    
+    # # Feature generation using most important features
+    # number_of_generated_feature = 7
+    # selector_train = DataSelector(df_train, df_train_label).features_selection(number_of_generated_feature)
+    # generator_train = DataGenerator(df_train,selector_train[1]).polynomial_features(3)    
+    # df_train = generator_train[0]
+    
+    # # Feature generation on test data using training data generator model
+    # selector_test = df_test[selector_train[1].columns]
+    # generator_test = DataGenerator(df_test,selector_test).polynomial_features(3)
+    # df_test = generator_test[0]
+    
+    # # Correlation Matrix
+    # DataVisualizator(df_train).correlation_matrix()
+    
+    # # Pie Chart
+    # DataVisualizator(df_train).pie_chart(columns_name=df_train.columns)
+    # # Bar Chart
+    # DataVisualizator(df_train).bar_chart(columns_name=df_train.columns, label=LABEL)
+    # # Histogram
+    # DataVisualizator(df_train).histogram(columns_name=df_train.columns)
+    # # Pair plot, plotting feature vs label
+    # DataVisualizator(df_train).pair_plot(label=LABEL)
+    # # Box plot
+    # DataVisualizator(df_train).box_plot(columns_name=df_train.columns,label=LABEL)
+    # # Violin Plot
+    # DataVisualizator(df_train).violin_plot(columns_name=df_train.columns,label=LABEL)
+    
+    
+    # # Scaling data
+    # scaler = DataScaler(df_train).scaler(method='yeo-johnson')
+    # df_train = pd.DataFrame(scaler[0])
+    # # Scaling data on test data using training data scaler model
+    # df_test = pd.DataFrame(scaler[1].transform(df_test))
+    
+    # #/home/serkhane/repo/test-quantmetry/results
+    # Modeler(df_train, LABEL).XGBoost_model(df_train=df_train,
+    #                                    df_train_label=df_train_label,
+    #                                    df_test=df_test,
+    #                                    df_test_label=df_test_label,
+    #                                    model_path="/home/serkhane/repo/test-quantmetry/results/test.model",
+    #                                    num_round=250,
+    #                                    threshold_class=0.3,
+    #                                    max_depth=2,
+    #                                    eta=0.1)
+    
+    # Modeler(df_train, LABEL).Ensemble_model(df_train=df_train,
+    #                                        df_train_label=df_train_label,
+    #                                        df_test=df_test,
+    #                                        df_test_label=df_test_label,
+    #                                        model_path="/home/serkhane/repo/test-quantmetry/results/test.model",
+    #                                        n_estimators=10000,
+    #                                        max_depth=None,
+    #                                        threshold_class=0.25,
+    #                                        method='RandomForest')
+
+
+if __name__ == "__main__":
+    
+    MODEL_NAME = "speech-recognition-cnn.model"
+    
+    directory_of_script = os.path.dirname(os.path.realpath(__file__))
+    directory_of_model = os.path.join(directory_of_script,"model")
+    directory_of_results = os.path.join(directory_of_script,"results")
+    directory_of_data = os.path.join(directory_of_script,"DATA","voice_commands")
+    os.makedirs(directory_of_model,exist_ok=True)
+    os.makedirs(directory_of_results,exist_ok=True)
+    os.makedirs(directory_of_data,exist_ok=True)
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-data_directory", help="Directory of location of the data for training", required=False,  default=directory_of_data, nargs='?')
+    # parser.add_argument("-path_model", help="Path of an existing model to use for prediction", required=False, default=os.path.join(directory_of_model,MODEL_NAME), nargs='?')
+    # parser.add_argument("-NaN_imputation_feature_scaling_PCA_usage", help="Apply or not NaN imputation, Feature Scaling and PCA", required=False, choices=["False","True"], default="False", nargs='?')
+    # parser.add_argument("-max_depth", help="Maximum depth of a tree. Increasing this value will make the model more complex and more likely to overfit. 0 is only accepted in lossguided growing policy when tree_method is set as hist or gpu_hist and it indicates no limit on depth. Beware that XGBoost aggressively consumes memory when training a deep tree. range: [0,∞] (0 is only accepted in lossguided growing policy when tree_method is set as hist or gpu_hist)", required=False, default=6, type=int, nargs='?')
+    # parser.add_argument("-eta", help="Step size shrinkage used in update to prevents overfitting. After each boosting step, we can directly get the weights of new features, and eta shrinks the feature weights to make the boosting process more conservative. range: [0,1]", required=False, default=0.3, type=float, nargs='?')
+    # parser.add_argument("-num_round", help="The number of rounds for boosting", required=False, default=100, type=int, nargs='?')
+    args = parser.parse_args()
+    
+    main(args)    
---
+++ modules/modeling/speech_recognition.py
@@ -0,0 +1,380 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# from datetime import datetime
+
+# import numpy as np
+
+# from sklearn.metrics import mean_squared_error
+# from sklearn.model_selection import GridSearchCV
+# from sklearn.model_selection import cross_val_score
+# from sklearn.model_selection import ShuffleSplit
+# from sklearn.metrics import confusion_matrix
+# import xgboost as xgb
+# from sklearn.ensemble import RandomForestRegressor
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn.ensemble import ExtraTreesClassifier
+# from sklearn.tree import DecisionTreeClassifier
+# from sklearn.linear_model import LinearRegression
+# from sklearn.preprocessing import PolynomialFeatures
+# from scipy.optimize import curve_fit
+# from scipy.interpolate import lagrange
+
+# from modules.Global.variable import Var
+
+# from modules.visualization.visualization import DataVisualizator
+
+
+class basic_CNN:
+    '''
+    Pour le modèle, vous utiliserez un simple réseau de neurones convolutifs (CNN), puisque 
+    vous avez transformé les fichiers audio en images de spectrogramme. Le modèle comporte 
+    également les couches de prétraitement supplémentaires suivantes :
+        
+        Une couche de Resizing pour sous-échantillonner l'entrée afin de permettre au modèle de 
+        s'entraîner plus rapidement.
+        Une Normalization couche de normaliser chaque pixel de l'image en fonction de son écart 
+        moyen et standard.
+        
+    Pour la couche de Normalization , sa méthode d' adapt devrait d'abord être appelée sur les données 
+    d'apprentissage afin de calculer les statistiques agrégées (c'est-à-dire la moyenne et l'écart type).
+    '''
+
+
+# class Modeler:
+#     '''
+#     Class used to create Machine Learning with data
+#     '''
+    
+#     SCRIPT_DIRECTORY = Var().SCRIPT_DIRECTORY
+#     MAX_NUMBER_OF_CATEGORICAL_OCCURENCES = Var().MAX_NUMBER_OF_CATEGORICAL_OCCURENCES
+#     LABEL_OBJECTIVE_REGRESSION = Var().LABEL_OBJECTIVE_REGRESSION
+#     LABEL_OBJECTIVE_CLASSIFICATION = Var().LABEL_OBJECTIVE_CLASSIFICATION
+#     METRIC_REGRESSION = Var().METRIC_REGRESSION
+#     METRIC_CLASSIFICATION = Var().METRIC_CLASSIFICATION 
+    
+#     def __init__(self, dataframe, label):
+#         self.dataframe = dataframe
+#         self.label = label
+        
+#     def _detect_objective(self, data_label):
+#         '''
+#         Detect which objective to choose for XGBoost model regarding values in label
+
+#         Parameters
+#         ----------
+#         data_label : dataframe
+            
+
+#         Returns
+#         -------
+#         str
+#             Name of the objective to use for the XGBoost model
+
+#         '''
+        
+#         length_unique_value = len(set(data_label))
+#         minimum = min(data_label)
+#         maximum = max(data_label)
+        
+#         if 3 <= length_unique_value <= self.MAX_NUMBER_OF_CATEGORICAL_OCCURENCES:
+#             # Multi classification
+#             print(self.LABEL_OBJECTIVE_CLASSIFICATION[0])
+#             return (self.LABEL_OBJECTIVE_CLASSIFICATION[0],self.METRIC_CLASSIFICATION[1])
+#         if length_unique_value <= 2:
+#             # Binary classification
+#             print(self.LABEL_OBJECTIVE_CLASSIFICATION[1])
+#             return (self.LABEL_OBJECTIVE_CLASSIFICATION[1],self.METRIC_CLASSIFICATION[0])
+#         if 3 <= length_unique_value and 0 <= minimum and maximum <= 1:
+#             # Logistic regression
+#             print(self.LABEL_OBJECTIVE_REGRESSION[1])
+#             return (self.LABEL_OBJECTIVE_REGRESSION[1],self.METRIC_REGRESSION[0])
+#         if self.MAX_NUMBER_OF_CATEGORICAL_OCCURENCES < length_unique_value:
+#             # Regression
+#             print(self.LABEL_OBJECTIVE_REGRESSION[0])
+#             return (self.LABEL_OBJECTIVE_REGRESSION[0],self.METRIC_REGRESSION[0])
+        
+#     def linear_regressor(self, x):
+#         '''
+#         Create linear regression
+
+#         Parameters
+#         ----------
+#         x : string
+#             column name of x
+#         y : string
+#             column name of y
+
+#         Returns
+#         -------
+#         str, plot
+#             Display the linear regression function
+
+#         '''
+#         X = self.dataframe[x].to_numpy().reshape(-1, 1)
+#         y = self.dataframe[self.label].to_numpy()
+#         reg = LinearRegression().fit(X, y)
+#         reg.score(X, y)
+#         print(reg.coef_)
+#         a = reg.coef_[0]
+#         b = reg.coef_[1]
+#         print("Linear regression: y = " + str(a) + "*X+" + str(b))
+
+#     def Ensemble_model(self,
+#                         df_train,
+#                         df_train_label,
+#                         df_test,
+#                         df_test_label,
+#                         model_path,
+#                         n_estimators=100,
+#                         max_depth=6,
+#                         objective=None,
+#                         threshold_class=None,
+#                         method='RandomForest'):
+#         '''
+#         Create Random Forest model
+
+#         Parameters
+#         ----------
+#         df_train : dataframe
+#             dataframe containing training data
+#         df_train_label : dataframe
+#             dataframe containing ttraining label data
+#         df_test : dataframe
+#             dataframe containing test data
+#         df_test_label : dataframe
+#             dataframe containing test label data
+#         path_model : string
+#             string containing the path of the model which will be saved.
+#         n_estimators int, default=100
+#             The number of trees in the forest.
+#         max_depth : int, optional
+#             Maximum depth of a tree. Increasing this value will make the model 
+#             more complex and more likely to overfit. 0 is only accepted in 
+#             lossguided growing policy when tree_method is set as hist or gpu_hist 
+#             and it indicates no limit on depth. Beware that XGBoost aggressively 
+#             consumes memory when training a deep tree.
+#             range: [0,∞] (0 is only accepted in lossguided growing policy when 
+#                           tree_method is set as hist or gpu_hist). The default is 6.
+
+#         Returns
+#         -------
+#         Save model and related plot and display them
+
+#         '''
+        
+#         print("Training model using: Random Forest")
+#         detected_objective = self._detect_objective(df_train_label)
+#         eval_metric = detected_objective[1]
+#         objective = objective or detected_objective[0]
+#         # X = df_train.append(df_test,ignore_index=True).to_numpy()
+#         # y = df_train_label.append(df_test_label,ignore_index=True).to_numpy()
+#         X_train = df_train.to_numpy()
+#         # X_test = df_test.to_numpy()
+#         y_train = df_train_label.to_numpy()
+#         # y_test = df_test_label.to_numpy()
+#         if method == 'RandomForest':
+#             param = {'n_estimators': n_estimators,
+#                      'max_depth': max_depth, 
+#                      'n_jobs' : -1,
+#                      'verbose' : 1}
+#             if objective in set(self.LABEL_OBJECTIVE_REGRESSION):
+#                 estimator = RandomForestRegressor()
+#             if objective in set(self.LABEL_OBJECTIVE_CLASSIFICATION):
+#                 estimator = RandomForestClassifier()
+#         estimator.set_params(**param)
+#         estimator.fit(X_train, y_train)
+#         #scores = cross_val_score(estimator, X, y, cv=5)
+        
+#         now = datetime.now()
+#         dt_string = now.strftime("%d_%m_%Y_%H_%M_%S")
+#         param_string = "_".join([key +"_" + str(param[key]) for key in param.keys()])
+#         model_name = param_string + "_" + dt_string
+#         #bst.save_model(model_path + "_" + model_name)
+#         print("Model is available here: " + model_path + "_" + model_name)
+        
+        
+#         '''
+#         Get the RandomForest model results and information
+#         '''
+        
+#         # Cross validation with 100 iterations to get smoother mean test and train
+#         # score curves, each time with 20% data randomly selected as a validation set.
+        
+#         # DataVisualizator.plot_learning_curve(estimator,
+#         #                     title="Learning Curves: " + method,
+#         #                     X=X,
+#         #                     y=y,
+#         #                     cv=ShuffleSplit(n_splits=10, test_size=0.2, random_state=0),
+#         #                     n_jobs=-1)
+               
+#         if eval_metric == self.METRIC_REGRESSION[0]:
+#             ypred = estimator.predict(df_test)
+#             RMSE = mean_squared_error(df_test_label, ypred, squared=False)
+#             print(eval_metric.upper() + ": %.4f" % RMSE)
+#         if eval_metric == self.METRIC_CLASSIFICATION[0]:
+#             ypred = estimator.predict_proba(df_test)[:,1]
+#             ypred_rounded = ypred.round()
+#             if len(set(df_train_label)) == 2:
+#                 ypred_rounded = np.where(ypred >= threshold_class,1,0)            
+#             cf_matrix = confusion_matrix(df_test_label,ypred_rounded)
+#             DataVisualizator.confusion_matrix(cf_matrix)
+#             DataVisualizator.roc_auc(np.array(df_test_label), ypred)
+            
+#         DataVisualizator(self.dataframe).features_importance(estimator=estimator, estimator_type='scikit-learn')
+        
+#         print("Training DONE")        
+    
+#     def XGBoost_model(self,
+#                          df_train,
+#                          df_train_label,
+#                          df_test,
+#                          df_test_label,
+#                          num_round,
+#                          max_depth=6,
+#                          eta=0.3,
+#                          num_class=None,
+#                          objective=None,
+#                          threshold_class=None,
+#                          model_path=None):
+#         '''
+#         Create XGBoost model
+
+#         Parameters
+#         ----------
+#         df_train : dataframe
+#             dataframe containing training data
+#         df_train_label : dataframe
+#             dataframe containing ttraining label data
+#         df_test : dataframe
+#             dataframe containing test data
+#         df_test_label : dataframe
+#             dataframe containing test label data
+#         path_model : string
+#             string containing the path of the model which will be saved.
+#         num_round : int
+#             The number of rounds for boosting.
+#         max_depth : int, optional
+#             Maximum depth of a tree. Increasing this value will make the model 
+#             more complex and more likely to overfit. 0 is only accepted in 
+#             lossguided growing policy when tree_method is set as hist or gpu_hist 
+#             and it indicates no limit on depth. Beware that XGBoost aggressively 
+#             consumes memory when training a deep tree.
+#             range: [0,∞] (0 is only accepted in lossguided growing policy when 
+#                           tree_method is set as hist or gpu_hist). The default is 6.
+#         eta : float
+#             Step size shrinkage used in update to prevents overfitting. After each 
+#             boosting step, we can directly get the weights of new features, and eta 
+#             shrinks the feature weights to make the boosting process more 
+#             conservative. The default is 0.3.
+#             range: [0,1]
+
+#         Returns
+#         -------
+#         Save model and related plot and display them
+
+#         '''
+        
+#         print("Training model using: XGBoost")
+#         detected_objective = self._detect_objective(df_train_label)
+#         eval_metric = detected_objective[1]
+#         objective = objective or detected_objective[0]
+#         dtrain = xgb.DMatrix(df_train, label=df_train_label)
+#         dtest = xgb.DMatrix(df_test, label=df_test_label)
+        
+#         evallist = [(dtest, 'eval'), (dtrain, 'train')]
+#         evals_result = {}    
+#         param = {'max_depth': max_depth, 
+#                  'eta': eta, 
+#                  'objective': objective, 
+#                  'nthread' : -1,
+#                  'num_class' : num_class}
+        
+#         bst = xgb.train(param, 
+#                         dtrain, 
+#                         num_round, 
+#                         evallist, 
+#                         early_stopping_rounds=10, 
+#                         evals_result=evals_result)
+        
+#         now = datetime.now()
+#         dt_string = now.strftime("%d_%m_%Y_%H_%M_%S")
+#         param_string = "_".join([key +"_" + str(param[key]) for key in param.keys()])
+#         model_name = param_string + "_" + dt_string
+#         #bst.save_model(model_path + "_" + model_name)
+#         #print("Model is available here: " + model_path + "_" + model_name)
+        
+        
+#         '''
+#         Get the XGBoost model results and information
+#         '''
+#         x_axis = range(len(evals_result['train'][eval_metric]))
+#         y_axis = [evals_result['train'][eval_metric], 
+#                   evals_result['eval'][eval_metric]]
+        
+#         DataVisualizator.curve(x=x_axis, 
+#                                y=y_axis,
+#                                xlabel='Number of Rounds',
+#                                ylabel=eval_metric.upper(),
+#                                title="XGBoost - " + eval_metric.upper(),
+#                                label=["train","eval"])
+        
+#         ypred = bst.predict(dtest)        
+#         if eval_metric == self.METRIC_REGRESSION[0]:
+#             RMSE = mean_squared_error(df_test_label, ypred, squared=False)
+#             print(eval_metric.upper() + ": %.4f" % RMSE)
+#         if eval_metric == self.METRIC_CLASSIFICATION[0]:
+#             ypred_rounded = ypred.round()
+#             if len(set(df_train_label)) == 2:
+#                 ypred_rounded = np.where(ypred >= threshold_class,1,0)            
+#             cf_matrix = confusion_matrix(df_test_label,ypred_rounded)
+#             DataVisualizator.confusion_matrix(cf_matrix)
+#             DataVisualizator.roc_auc(np.array(df_test_label), ypred)
+            
+#         DataVisualizator(self.dataframe).features_importance(estimator=bst, estimator_type='xgboost')
+#         print("Training DONE")
+#         return bst
+        
+        
+#     def hyperParameterTuningXGB(self, X_train, y_train, objective):
+#         '''
+#         Grid search best parameters to fit the model
+
+#         Parameters
+#         ----------
+#         X_train : xgb.DMatrix
+#             Array containing training data.
+#         y_train : xgb.DMatrix
+#             Array containing test data.
+
+#         Returns
+#         -------
+#         dict
+#             Best paramaters.
+
+#         '''
+        
+#         param_tuning = {
+#             'learning_rate': [0.05, 0.10, 0.15, 0.20, 0.25, 0.30],
+#             'max_depth': [3, 4, 5, 6, 8, 10, 12, 15],
+#             'min_child_weight': [1, 3, 5, 7],
+#             'subsample': [0.5, 0.7, 1],
+#             'colsample_bytree': [0.1, 0.3, 0.4, 0.5 , 0.7, 1],
+#             'n_estimators' : [100, 200, 500],
+#             'gamma' : [0.0, 0.1, 0.2 , 0.3, 0.4],
+#             'objective': [objective]
+#         }
+    
+#         xgb_model = xgb.XGBRegressor()
+    
+#         gsearch = GridSearchCV(estimator = xgb_model,
+#                                param_grid = param_tuning,                        
+#                                #scoring = 'neg_mean_absolute_error', #MAE
+#                                #scoring = 'neg_mean_squared_error',  #MSE
+#                                cv = 5,
+#                                n_jobs = -1,
+#                                verbose = 1)
+    
+#         gsearch.fit(X_train,y_train)
+    
+#         return gsearch.best_params_
