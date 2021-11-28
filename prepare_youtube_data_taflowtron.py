47a762a8bb0406ce38fa5241b81f20761ef07a2c
serkhanekarim <karimserkhane@outlook.fr>
2021-11-28 11:12:41 +0400

    update youtube crawler for taflowtron, update fileliste and youtube scraping list

--- prepare_youtube_data_taflowtron.py
+++ prepare_youtube_data_taflowtron.py
@@ -54,13 +54,16 @@ def main(args):
     AUDIO_FORMAT = 'wav' #Required audio format for taflowtron
        
     path_list_url = args.path_list_url
-    language = args.language
+    language = args.language.lower()
     data_directory = args.data_directory
     path_youtube_cleaner = args.path_youtube_cleaner
     directory_taflowtron_filelist = args.directory_taflowtron_filelist
-    converter = args.converter
+    converter = args.converter.lower() == "true"
     path_hparam_file = args.path_hparam_file
     concatenate_vtt = args.concatenate_vtt.lower() == "true"
+    silence = args.silence.lower()
+    name_train_param_config = args.name_train_param_config
+    name_data_config = args.name_data_config
     silence_threshold = int(args.silence_threshold)
     max_limit_duration = int(args.max_limit_duration)
     min_limit_duration = int(args.min_limit_duration)
@@ -119,33 +122,32 @@ def main(args):
         '''
         Add and/or Remove leading and trailing silence and/or convert audio
         '''
+        if silence == "add":
+            print("Revoming leading and trailing silence and convert audio...")
+            preprocess_audio(file_list=list_trimmed_audio_path,silence_audio_size=0)
         
-        # print("Add and/or Revoming leading and trailing silence and/or Convert audio...")
-        # preprocess_audio(file_list=list_trimmed_audio_path,
-        #                   silence_audio_size=0)
+            # [AudioPreprocessor().remove_lead_trail_audio_wav_silence(path_input=trimmed_audio_path, 
+            #                                                          path_output=trimmed_audio_path,
+            #                                                          silence_threshold=silence_threshold) for trimmed_audio_path in list_trimmed_audio_path]
         
-        # [AudioPreprocessor().remove_lead_trail_audio_wav_silence(path_input=trimmed_audio_path, 
-        #                                                          path_output=trimmed_audio_path,
-        #                                                          silence_threshold=silence_threshold) for trimmed_audio_path in list_trimmed_audio_path]
-        
-        [AudioPreprocessor().add_lead_trail_audio_wav_silence(path_input=trimmed_audio_path, 
-                                                                  path_output=trimmed_audio_path,
-                                                                  silence_duration=500,
-                                                                  before=True, 
-                                                                  after=True) for trimmed_audio_path in list_trimmed_audio_path]
+        if silence == "remove":
+            print("Padding silence...")
+            [AudioPreprocessor().add_lead_trail_audio_wav_silence(path_input=trimmed_audio_path, 
+                                                                      path_output=trimmed_audio_path,
+                                                                      silence_duration=500,
+                                                                      before=True, 
+                                                                      after=True) for trimmed_audio_path in list_trimmed_audio_path]
         
         '''
         Convert audio data for taflowtron model
         '''
-        if converter.lower() == 'true':
+        if converter and silence != "add":
             print("Audio conversion...")
-            list_total_new_audio_path = []
             dir_audio_data_files_converted = os.path.join(data_directory,language,filename,'clips_converted')
             os.makedirs(dir_audio_data_files_converted,exist_ok=True)
             for new_audio_path in tqdm(list_trimmed_audio_path):
                 filename = Method().get_filename(new_audio_path)
                 path_converted_audio = os.path.join(dir_audio_data_files_converted,filename + "." + AUDIO_FORMAT)
-                list_total_new_audio_path.append(path_converted_audio)
                 if not os.path.isfile(path_converted_audio):
                     AudioPreprocessor().convert_audio(path_input=new_audio_path, 
                                                       path_output=path_converted_audio,
@@ -153,18 +155,13 @@ def main(args):
                                                       channel=1, 
                                                       bits=16)
             shutil.rmtree(dir_audio_data_files)
-            os.rename(dir_audio_data_files_converted,dir_audio_data_files)
-        
-        else:
-            #Get a full list of all path audio and subtitles for taflowtron filelist
-            list_total_new_audio_path = list_trimmed_audio_path
-        
+            os.rename(dir_audio_data_files_converted,dir_audio_data_files)        
         
         '''
         Get ITN symbols from subtitles
         '''
         print("Getting ITN symbols from data...")
-        ITN_symbols += DataPreprocessor().get_ITN_data(data_text=list_subtitle, data_option=list_total_new_audio_path)
+        ITN_symbols += DataPreprocessor().get_ITN_data(data_text=list_subtitle, data_option=list_trimmed_audio_path)
     
         '''
         Create taflowtron filelist and data information
@@ -172,7 +169,7 @@ def main(args):
         list_duration = [(time[1]-time[0])/1000 for time in list_time]
         list_total_video_extraction = [sum(list_duration)/3600]*len(list_duration)
         total_set_audio_length += sum(list_duration)
-        mem_info = pd.DataFrame({"Audio Path":list_total_new_audio_path,
+        mem_info = pd.DataFrame({"Audio Path":list_trimmed_audio_path,
                                  "Text":list_subtitle,
                                  "Speaker ID":voice_id,
                                  "Duration (seconds)":list_duration,
@@ -181,7 +178,7 @@ def main(args):
         
         data_information = data_information.append(mem_info)
         
-        data_filelist += [list_total_new_audio_path[index] + "|" + subtitle + "|" + str(voice_id) for index,subtitle in enumerate(list_subtitle)]
+        data_filelist += [list_trimmed_audio_path[index] + "|" + subtitle + "|" + str(voice_id) for index,subtitle in enumerate(list_subtitle)]
         if nb_speaker > 1: voice_id += 1
         
     
@@ -219,10 +216,10 @@ def main(args):
     DataWriter(ITN_symbols, path_ITN_symbols).write_data_file()
     DataWriter(data_information, path_data_information, header=True).write_data_file()
 
+    '''
+    Update hparams with filelist and batch size
+    '''
     if path_hparam_file is not None:
-        '''
-        Update hparams with filelist and batch size
-        '''
         data_haparams = DataReader(path_hparam_file).read_data_file()
         data_haparams = DataWriter(data_haparams, path_hparam_file).write_edit_data(key='        "training_files": ', value = '"' + path_train_filelist + '",\n')
         data_haparams = DataWriter(data_haparams, path_hparam_file).write_edit_data(key='        "validation_files": ', value = '"' + path_valid_filelist + '",\n')
@@ -245,17 +242,21 @@ if __name__ == "__main__":
     parser.add_argument("-path_list_url", help="Path of a file containing Youtube urls", required=True, nargs='?')
     parser.add_argument("-language", help="Language to select for subtitle", required=True, nargs='?')
     parser.add_argument("-data_directory", help="Directory of location of the data for training", required=False, default=directory_of_data, nargs='?')
-    parser.add_argument("-path_youtube_cleaner", help="Path of a tsv file containing regex substitution", nargs='?')
+    parser.add_argument("-path_youtube_cleaner", help="Path of a tsv file containing regex substitution", required=True, nargs='?')
     parser.add_argument("-directory_taflowtron_filelist", help="Directory of the file containing information of user voice splitted for Taflowtron training", required=True, nargs='?')
-    parser.add_argument("-path_hparam_file", help="Path of the file containing the training paramaters", nargs='?')
-    parser.add_argument("-path_symbols_file", help="Path of the file containing the symbols", nargs='?')
-    parser.add_argument("-batch_size", help="Number of batch size", nargs='?')
+    parser.add_argument("-path_hparam_file", help="Path of the file containing the training paramaters", required=False, nargs='?')
+    parser.add_argument("-path_symbols_file", help="Path of the file containing the symbols", required=False, nargs='?')
+    parser.add_argument("-batch_size", help="Number of batch size", required=False, nargs='?')
     parser.add_argument("-converter", help="Convert or not the audio downliaded from youtube", required=True, nargs='?')
     parser.add_argument("-concatenate_vtt", help="Concatenate subtitles/sentences to avoid small or cut audio subtitles/sentences", required=True, nargs='?')
+    parser.add_argument("-silence", help="add or remove silence", required=False, choices=['add', 'remove'],nargs='?')
     parser.add_argument("-silence_threshold", help="Silence threshold in dFBS,lower the value is, les it'll remove the silence", required=False, nargs='?')
     parser.add_argument("-max_limit_duration", help="Maximum length authorized of an audion in millisecond", required=False, nargs='?')
     parser.add_argument("-min_limit_duration", help="Minimum length authorized of an audion in millisecond", required=False, nargs='?')
     parser.add_argument("-nb_speaker", help="Number of speaker", required=False, default=0, nargs='?')
+    parser.add_argument("-name_train_param_config", help="Name of the experimentation of the training parameters configuration", required=False, default=0, nargs='?')
+    parser.add_argument("-name_data_config", help="Name of the experimentation of the training data configuration", required=False, default=0, nargs='?')
+    
     
     
     args = parser.parse_args()
