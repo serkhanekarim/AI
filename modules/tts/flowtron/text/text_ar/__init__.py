06a3b6b44805ff6c2db97ea851fbb1192c2b6856
serkhanekarim <karimserkhane@outlook.fr>
2021-12-31 00:05:04 +0400

    fix issue in flowtron globalization language 8

--- modules/tts/flowtron/text/text_ar/__init__.py
+++ modules/tts/flowtron/text/text_ar/__init__.py
@@ -1,8 +1,8 @@
 """ from https://github.com/keithito/tacotron """
 import re
-from text_ar import cleaners
-from text_ar.symbols import symbols
-from text_ar.symbols import _punctuation as punctuation_symbols
+from text.text_ar import cleaners
+from text.text_ar.symbols import symbols
+from text.text_ar.symbols import _punctuation as punctuation_symbols
 
 # Mappings from symbol to numeric ID and vice versa:
 _symbol_to_id = {s: i for i, s in enumerate(symbols)}
