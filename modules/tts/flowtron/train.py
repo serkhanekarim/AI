a715bc9e67ec780c2740703a825981129d3a737d
serkhanekarim <karimserkhane@outlook.fr>
2021-12-31 00:39:00 +0400

    fix issue in flowtron globalization language 12

--- modules/tts/flowtron/train.py
+++ modules/tts/flowtron/train.py
@@ -402,7 +402,7 @@ if __name__ == "__main__":
     model_config = config["model_config"]
     
     exec("from data" + "_" + data_config["language"] + " import Data")
-    exec("from data" + "_" + data_config["language"] + " import DataCollactet")
+    exec("from data" + "_" + data_config["language"] + " import DataCollacte")
 
     # Make sure the launcher sets `RANK` and `WORLD_SIZE`.
     rank = int(os.getenv('RANK', '0'))
