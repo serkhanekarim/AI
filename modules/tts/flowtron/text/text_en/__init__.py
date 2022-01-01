8cfd38dc40edb11e99569729df71700f625a63ab
serkhanekarim <karimserkhane@outlook.fr>
2022-01-01 17:36:11 +0400

    update config json with new flowtron format

--- modules/tts/flowtron/text/text_en/__init__.py
+++ modules/tts/flowtron/text/text_en/__init__.py
@@ -1,8 +1,8 @@
 """ from https://github.com/keithito/tacotron """
 import re
-from text_en import cleaners
-from text_en.symbols import symbols
-from text_en.symbols import _punctuation as punctuation_symbols
+from text.text_en import cleaners
+from text.text_en.symbols import symbols
+from text.text_en.symbols import _punctuation as punctuation_symbols
 
 # Mappings from symbol to numeric ID and vice versa:
 _symbol_to_id = {s: i for i, s in enumerate(symbols)}
